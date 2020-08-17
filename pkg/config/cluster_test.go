5aac5677d4407730bf771c359bcfb23d3c649cbf
Benjamin Yolken <benjamin.yolken@segment.com>
2020-08-16 21:18:18 -0700

    Add cluster test

---
+++ pkg/config/cluster_test.go
@@ -0,0 +1,91 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClusterValidate(t *testing.T) {
+	type testCase struct {
+		description   string
+		clusterConfig ClusterConfig
+		expError      bool
+	}
+
+	testCases := []testCase{
+		{
+			description: "all good",
+			clusterConfig: ClusterConfig{
+				Meta: ClusterMeta{
+					Name:        "test-cluster",
+					Region:      "test-region",
+					Environment: "test-environment",
+					Description: "test-description",
+				},
+				Spec: ClusterSpec{
+					BootstrapAddrs: []string{"broker-addr"},
+					ZKAddrs:        []string{"zk-addr"},
+					VersionMajor:   "v2",
+				},
+			},
+			expError: false,
+		},
+		{
+			description: "missing meta fields",
+			clusterConfig: ClusterConfig{
+				Meta: ClusterMeta{
+					Environment: "test-environment",
+					Description: "test-description",
+				},
+				Spec: ClusterSpec{
+					BootstrapAddrs: []string{"broker-addr"},
+					ZKAddrs:        []string{"zk-addr"},
+					VersionMajor:   "v2",
+				},
+			},
+			expError: true,
+		},
+		{
+			description: "missing bootstrap addresses",
+			clusterConfig: ClusterConfig{
+				Meta: ClusterMeta{
+					Name:        "test-cluster",
+					Region:      "test-region",
+					Environment: "test-environment",
+					Description: "test-description",
+				},
+				Spec: ClusterSpec{
+					ZKAddrs:      []string{"zk-addr"},
+					VersionMajor: "v2",
+				},
+			},
+			expError: true,
+		},
+		{
+			description: "missing zk addresses",
+			clusterConfig: ClusterConfig{
+				Meta: ClusterMeta{
+					Name:        "test-cluster",
+					Region:      "test-region",
+					Environment: "test-environment",
+					Description: "test-description",
+				},
+				Spec: ClusterSpec{
+					BootstrapAddrs: []string{"broker-addr"},
+					VersionMajor:   "v2",
+				},
+			},
+			expError: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := testCase.clusterConfig.Validate()
+		if testCase.expError {
+			assert.Error(t, err, testCase.description)
+		} else {
+			assert.NoError(t, err, testCase.description)
+		}
+	}
+}
