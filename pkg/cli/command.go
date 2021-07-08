7da57c9f1796a573b6d167026d8ed374db672427
Benjamin Yolken <benjamin.yolken@segment.com>
2021-07-08 15:43:00 -0700

    Update command parsing

--- pkg/cli/command.go
+++ pkg/cli/command.go
@@ -7,6 +7,19 @@ type cliCommand struct {
 	flags map[string]string
 }
 
+func (c cliCommand) getBoolValue(key string) bool {
+	value, ok := c.flags[key]
+
+	if value == "true" {
+		return true
+	} else if value == "" && ok {
+		// If key is set but value is not, treat this as "true"
+		return true
+	} else {
+		return false
+	}
+}
+
 // parse parses the CLI inputs into command args and flags.
 func parseInputs(input string) cliCommand {
 	args := []string{}
--- pkg/cli/command_test.go
+++ pkg/cli/command_test.go
@@ -35,3 +35,17 @@ func TestParse(t *testing.T) {
 		parseInputs("arg1 arg2 --flag1=value1 arg3 --flag2=value2"),
 	)
 }
+
+func TestGetBoolValue(t *testing.T) {
+	command := cliCommand{
+		flags: map[string]string{
+			"key1": "",
+			"key2": "true",
+			"key3": "false",
+		},
+	}
+	assert.True(t, command.getBoolValue("key1"))
+	assert.True(t, command.getBoolValue("key2"))
+	assert.False(t, command.getBoolValue("key3"))
+	assert.False(t, command.getBoolValue("non-existent-key"))
+}
